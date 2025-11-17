f45d4cc751e660d8473d5131d0197f424e35232e
Administrator <gitlab_admin_371d15@example.com>
2025-11-17 21:02:49 +0200

    Remove commented-out code from application.py

--- page_objects/application.py
+++ page_objects/application.py
@@ -7,7 +7,6 @@ class App:
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
         self.base_url = base_url
-        # self.page.goto("http://127.0.0.1:8000/login/?next=/")
 
     def goto(self, endpoint: str, use_base_url = True):
         if use_base_url:
@@ -21,7 +20,6 @@ class App:
 
 
     def login(self, login: str, password: str):
-        #self.page.get_by_role("textbox", name="Username:").click()
         self.page.get_by_role("textbox", name="Username:").fill(login)
         #self.page.get_by_role("textbox", name="Password:").click()
         self.page.get_by_role("textbox", name="Password:").fill(password)
@@ -29,16 +27,9 @@ class App:
 
 
     def create_test(self, test_name: str, test_description: str):
-        #self.page.get_by_role("link", name="Create new test").click()
-        #self.page.locator("#id_name").click()
         self.page.locator("#id_name").fill(test_name)
-        #self.page.get_by_role("textbox", name="Test description").click()
         self.page.get_by_role("textbox", name="Test description").fill(test_description)
         self. page.get_by_role("button", name="Create").click()
-
-
-    # def open_tests(self):
-    #     self.page.get_by_role("link", name="Test Cases").click()
 
 
     def check_test_exists(self, test_name: str):
@@ -46,8 +37,6 @@ class App:
 
 
     def delete_test_by_name(self, test_name: str):
-        #self.page.locator(".ttRem.deleteBtn.delete_22").click()
-        #self.page.click("//tr[13]/td[9]/button[normalize-space(.)='Delete']")
         row = self.page.query_selector(f'*css=tr >> text=\"{test_name}\"')
         row.query_selector('.deleteBtn').click()
 

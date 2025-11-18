4900f921987422a5b688b14069624afdb3924ab9
Administrator <gitlab_admin_371d15@example.com>
2025-11-18 19:16:03 +0200

    Refactor test case creation by using parameterized tests for improved readability and coverage

--- tests/test_testcases.py
+++ tests/test_testcases.py
@@ -1,28 +1,25 @@
+from pytest import mark
 
-def test_new_testcase(desktop_app_auth):
-    test_name = 'hello'
+
+ddt = {
+    'argnames': 'name,description',
+    'argvalues': [
+        ('hello', 'world'),
+        ('hello', ''),
+        ('123', 'world'),
+    ],
+    'ids': ['general test', 'test with no description', 'test with digits']
+}
+
+@mark.parametrize(**ddt)
+def test_new_testcase(desktop_app_auth, name, description):
     desktop_app_auth.navigate_to('Create new test')
-    desktop_app_auth.create_test(test_name, 'world')
+    desktop_app_auth.create_test(name, description)
     desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
+    assert desktop_app_auth.test_cases.check_test_exists(name)
+    desktop_app_auth.test_cases.delete_test_by_name(name)
 
 
-def test_new_testcase_no_descr(desktop_app_auth):
-    test_name = 'hello'
-    desktop_app_auth.navigate_to('Create new test')
-    desktop_app_auth.create_test(test_name, '')
-    desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
 
-
-def test_new_testcase_digits_name(desktop_app_auth):
-    test_name = '123'
-    desktop_app_auth.navigate_to('Create new test')
-    desktop_app_auth.create_test(test_name, 'world')
-    desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
 
 
